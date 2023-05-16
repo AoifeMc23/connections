cf6755709a400126a7d0eb0bc52ab97f84854902
AoifeMc23 <132588929+AoifeMc23@users.noreply.github.com>
2023-05-16 09:43:48 +0100

    updata

--- sketch.js
+++ sketch.js
@@ -1,3 +1,91 @@
+function setup() {
+  var canvas1 = createCanvas(windowWidth, windowHeight);
+  canvas1.position(0, 0);
+
+  canvas1.parent("#canvas1");
+
+  colorMode(HSB, 360, 100, 100, 100);
+
+  noLoop();
+}
+
+var mouseOn = document.getElementById("mouse_on");
+var mouseOff = document.getElementById("mouse_off");
+
+mouseOff.style.display = "none";
+
+function mouseTurnOn() {
+  if ((mouseOff.style.display = "none")) {
+    mouseOn.style.display = "none";
+    mouseOff.style.display = "block";
+    loop();
+  }
+}
+
+function mouseTurnOff() {
+  if ((mouseOff.style.display = "block")) {
+    mouseOn.style.display = "block";
+    mouseOff.style.display = "none";
+    clear();
+    noLoop();
+  } else {
+    mouseOff.style.display = "block";
+    loop();
+  }
+}
+
+function draw() {
+  noStroke();
+
+  drawingContext.shadowBlur = 1000;
+  drawingContext.shadowColor = (207, 7, 99);
+
+  ellipse(mouseX, mouseY, 33, 33);
+
+  fill(255, 255, 100, 4);
+  // for (i = 0; i < 100; i++) {
+  //   ellipse(200, 200, i * 3);
+  // }
+
+  blendMode(OVERLAY);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// var s1 = function (sketch) {
+//   sketch.setup = function () {
+//     var canvas1 = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+//     canvas1.position(0, 0);
+
+//     canvas1.parent("#canvas1");
+//   };
+
+//   var mouseOn = sketch.documnet.getElementById("mouse_on");
+//   var mouseOff = sketch.documnet.getElementById("mouse_off");
+
+//   sketch.mouseTurnOn = function () {
+//     if (mouseOn.style.display === block && mouseOff.style.display === none) {
+//       alert();
+//     }
+//   };
+
+//   sketch.draw = function () {
+//     sketch.ellipse(sketch.mouseX, sketch.mouseY, 33, 33);
+//     sketch.stroke(0);
+//     sketch.fill(150, 0, 0, 63);
+
+//     sketch.blendMode(sketch.MULTIPLY);
+//   };
+
+//   sketch.windowResized = function () {
+//     sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
+//   };
+// };
+
+// new p5(s1);
+
 // var s1 = function (sketch) {
 //   sketch.setup = function () {
 //     var canvas1 = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
